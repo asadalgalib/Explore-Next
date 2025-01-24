ff24111876b422c19e463a2b524612d4daea1085
unknown <asadalgalib9@gmail.com>
2025-01-24 18:03:16 +0600

    maybe finale test

--- src/Components/Navbar/page.js
+++ src/Components/Navbar/page.js
@@ -8,14 +8,14 @@ const Navbar = async () => {
     const { isAuthenticated } = getKindeServerSession();
     const isUserAuthenticated = await isAuthenticated();
     return (
-        <div className="bg-indigo-500 px-4 md:px-8 lg:px-16 py-4">
-            <div className=" flex items-center justify-between">
+        <div className="bg-indigo-500 px-4 md:px-8 lg:px-20">
+            <div className=" flex items-center justify-between py-[15px]">
                 <div className="flex-1">
                     <h1 className="text-2xl font-semibold text-white">Blog</h1>
                 </div>
-                <div className="flex items-center gap-4 justify-end">
+                <div className="flex items-center justify-end">
                     <div className="">
-                        <ul className="flex items-center gap-4 font-medium">
+                        <ul className="flex items-center gap-[15px] font-medium">
                             <Link href="/"><li className="font-medium text-white px-4 py-[6px] rounded-md bg-sky-500">Home</li></Link>
                             <Link href="/profile"><li className="font-medium text-white px-4 py-[6px] rounded-md bg-sky-500">Profile</li></Link>
                             {
